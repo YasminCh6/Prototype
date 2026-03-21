16e1c34f4983aea000471f8fa219480600d924e5
YasminCh6 <Yasmin.cassar.h63336@mcast.edu.mt>
2026-03-21 16:50:19 +0100

    movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
+    // Public -> Accessor
+    // String -> Data type
+    public Rigidbody myRigidbody;
+    public float movementSpeed = 5.5f;
+    public float rotationSpeed = 180f;
+    
     // Update is called once per frame
     void Update()
     {
+        // A basic condition to check
+        // for the W key
+        if (Keyboard.current.wKey.isPressed)
+        {
+            // Push the rigidbody up
+            myRigidbody.AddForce(transform.up * movementSpeed * Time.deltaTime);
+        }
+
+        if (Keyboard.current.sKey.isPressed)
+        {
+            // Push the rigidbody down
+            myRigidbody.AddForce(transform.up * -1 * movementSpeed * Time.deltaTime);
+        }
+
+        if (Keyboard.current.aKey.isPressed)
+        {
+            myRigidbody.AddTorque(Vector3.forward * rotationSpeed * Time.deltaTime);
+        }
+        
+        if (Keyboard.current.dKey.isPressed)
+        {
+            myRigidbody.AddTorque(Vector3.back * rotationSpeed * Time.deltaTime);
+        }
+        
+        // Accesses the Transform component
+        // transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
+        
         
     }
 }
